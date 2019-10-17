af5ba11d73cde06464a90f0daa8c31bdd480cf0f
Alex Snell <alexksnell@gmail.com>
2019-10-16 20:14:44 -0400

    Initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/asnell/client-server
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", greet)
+	log.Println("Starting Server :8080")
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal("Server startup failed!")
+	}
+}
